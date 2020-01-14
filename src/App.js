13610875dd02d59ff151cd08df8587d10c71d214
emphillips <matt-phillips@live.com>
2020-01-13 20:49:55 -0600

    Created Git for class project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+  super(props)
+  
+  this.state  = {
+    posts: [],
+    }
+  }
+// fetching data from api url
+
+async componentDidMount() {
+  const response = await fetch(`https://randomuser.me/api?results=25`);
+  const json = await response.json();
+  console.log(json)
+  this.setState({ posts: json.results });
 }
+
+render() {
+    return (
+      // console.log(this.state.posts.results)
+      <div>
+      {this.state.posts.map((post) => {    
+          return <div key={post.results}>
+            <h2>{post.results.name}</h2>
+              <p>{post.results.title}</p>
+            </div>
+         })}
+      </div>
+        )
+      }
+    }
 
 export default App;
